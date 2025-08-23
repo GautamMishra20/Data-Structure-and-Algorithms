6f984a065449e3479eee2b98418ad3dcb829cc7f
Gautam Mishra <g20autammishra1234@gmail.com>
2025-08-23 08:13:26 +0000

    trappingRainWater

--- Array/trappingRainWater.cpp
+++ Array/trappingRainWater.cpp
@@ -1,1 +1,35 @@
 // Number of units of water trapped ...
+class Solution {
+  public:
+    vector<int> getLeftMax(vector<int> &arr, int &n){
+        vector<int> leftMax(n);
+        leftMax[0] = arr[0];
+        for(int i=1; i<n; i++){
+            leftMax[i] = max(leftMax[i-1], arr[i]);
+        }
+        return leftMax;
+    }
+    vector<int> getRightMax(vector<int> &arr, int &n){
+        vector<int> rightMax(n);
+        rightMax[0] = arr[0];
+        for(int i=1; i<n; i++){
+            rightMax[i] = max(rightMax[i-1], arr[i]);
+        }
+        return rightMax;
+    }
+    int maxWater(vector<int> &arr) {
+        int n = arr.size();
+        
+        vector<int> leftMax = getLeftMax(arr, n);
+        vector<int> rightMax = getRightMax(arr, n);
+        
+        int sum = 0;
+        
+        for(int i = 0; i<n; i++){
+            int height = min(leftMax[i], rightMax[i]) - arr[i];
+            sum += height;
+        }
+        
+        return sum;
+    }
+};
