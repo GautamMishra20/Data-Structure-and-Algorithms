0bfdf6b2bd5bab7c8e822a416bfd12a4c6a29d07
Gautam Mishra <g20autammishra1234@gmail.com>
2025-08-24 04:34:09 +0000

    tabulation

--- Dynamic-Programming/03UniquePath.cpp
+++ Dynamic-Programming/03UniquePath.cpp
@@ -14,3 +14,45 @@ public:
         return recursion(m-1, n-1);
     }
 };
+
+// Memoization
+class Solution {
+public:
+    int memoization(int i, int j, vector<vector<int>> &dp){
+        if(i==0 && j==0) return 1;
+        if(i<0 || j<0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int up = memoization(i-1, j, dp);
+        int left = memoization(i, j-1, dp);
+
+        return dp[i][j] = up+left;
+    }
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int> (n,-1));
+        return memoization(m-1, n-1, dp);
+    }
+};
+
+
+// Tabulation
+int uniquePaths(int m, int n) {
+        int dp[m][n];
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 && j==0) dp[i][j] = 1;
+                else{
+                    int up = 0;
+                    int left = 0;
+                    if(i>0) up = dp[i-1][j];
+                    if(j>0) left = dp[i][j-1];
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+
+        return dp[m-1][n-1];
+    }
+};
